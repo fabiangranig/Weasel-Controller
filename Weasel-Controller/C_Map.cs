35ef8bd6eec32a6a8b6d2e29088c850b06a90072
Fabian <fabiangranig.fg@gmail.com>
2023-03-21 12:57:32 +0100

    Show all points for navigation / fixes #4

--- Weasel-Controller/C_Map.cs
+++ Weasel-Controller/C_Map.cs
@@ -10,18 +10,20 @@ namespace Weasel_Controller
     {
         //Head Node
         private C_Waypoint _Head;
+        private Random r1;
 
         //Constructor
         public C_Map()
         {
             _Head = null;
+            r1 = new Random();
         }
 
         //Methods
         public void AddNodeToNumber(C_Waypoint waypoint1, int id1)
         {
             //When there is nothing in the map
-            if(_Head == null)
+            if (_Head == null)
             {
                 _Head = waypoint1;
                 return;
@@ -32,25 +34,25 @@ namespace Weasel_Controller
 
         private void AddNodeToNumberBackend(C_Waypoint temp_map, C_Waypoint waypoint1, int id1)
         {
-            if(temp_map._Next != null && temp_map._PointId == id1)
+            if (temp_map._Next != null && temp_map._PointId == id1)
             {
                 temp_map._Next.Add(waypoint1);
                 return;
             }
 
-            if(temp_map._Next == null && temp_map._PointId == id1)
+            if (temp_map._Next == null && temp_map._PointId == id1)
             {
                 temp_map._Next = new List<C_Waypoint>();
                 temp_map._Next.Add(waypoint1);
             }
 
-            if(temp_map._Next == null)
+            if (temp_map._Next == null)
             {
                 return;
             }
 
             //Next lists
-            for(int i = 0; i < temp_map._Next.Count; i++)
+            for (int i = 0; i < temp_map._Next.Count; i++)
             {
                 AddNodeToNumberBackend(temp_map._Next[i], waypoint1, id1);
             }
@@ -73,13 +75,13 @@ namespace Weasel_Controller
                 return null;
             }
 
-            if(header._Next != null)
+            if (header._Next != null)
             {
                 C_Waypoint[] waypoints = new C_Waypoint[header._Next.Count];
-                for(int i = 0; i < waypoints.Length; i++)
+                for (int i = 0; i < waypoints.Length; i++)
                 {
                     waypoints[i] = FindWayPointBackend(header._Next[i], id1);
-                    if(waypoints[i] != null)
+                    if (waypoints[i] != null)
                     {
                         return waypoints[i];
                     }
@@ -94,12 +96,34 @@ namespace Weasel_Controller
         {
             C_Waypoint waypoint = FindWayPointBackend(_Head, start_point);
 
-            if(waypoint._Next == null)
+            if (waypoint._Next == null)
             {
                 waypoint._Next = new List<C_Waypoint>();
             }
 
             waypoint._Next.Add(FindWayPointBackend(_Head, end_point));
+        }
+
+        public void FreePath(int start, int end)
+        {
+            C_Waypoint start_position = FindWayPoint(start);
+            Console.WriteLine(FreePathBackend(start_position, end, ""));
+        }
+
+        public string FreePathBackend(C_Waypoint way, int id, string path)
+        {
+            if(way._Next == null)
+            {
+                return FreePathBackend(_Head, id, path + way._PointId + " ");
+            }
+
+            if(way._PointId == id)
+            {
+                return path + way._PointId;
+            }
+
+            C_Waypoint RandomWay = way._Next[r1.Next(0, way._Next.Count)];
+            return FreePathBackend(RandomWay, id, path + way._PointId + " ");
         }
     }
 }
