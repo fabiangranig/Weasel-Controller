f274a433fae1c455673491e60f7436e3a74bd1fd
Fabian <fabiangranig.fg@gmail.com>
2023-04-24 22:17:25 +0200

    Added first phone implementation

---
+++ Weasel-App/WeaselApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeaselApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private IPAddress IP;
+        private TcpClient Client;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SetIP(object sender, EventArgs e)
+        {
+            Entry IP_TextBox = (Entry)FindByName("IP_TextBox");
+            IP = IPAddress.Parse(Convert.ToString(IP_TextBox.Text));
+            Client = new TcpClient();
+            Client.Connect(IP, 26000);
+        }
+
+        public void SendWeaselToPosition(object sender, EventArgs e)
+        {
+            Entry PositionToSendTo = (Entry)FindByName("PositionToSendTo");
+            string userInput = PositionToSendTo.Text;
+            EnteredPosition.Text = userInput;
+            SendWeaselOverTCP(Int32.Parse(userInput));
+        }
+
+        public void SendWeaselOverTCP(int position)
+        {
+            Stream MessageStream = Client.GetStream();
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            var input = "MC6" + ":" + Convert.ToString(position);
+            if (input != null)
+            {
+                byte[] buffer = encoder.GetBytes(input);
+                MessageStream.Write(buffer, 0, buffer.Length);
+                MessageStream.Flush();
+            }
+        }
+    }
+}
